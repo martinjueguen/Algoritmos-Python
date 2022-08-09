c559ed58d477bdddf68852d56e61730d0c42ce1e
Tatincho <110904264+martinjueguen@users.noreply.github.com>
2022-08-09 11:44:13 -0300

    Ejercicios Python

---
+++ condicional1.py
@@ -0,0 +1,9 @@
+from tkinter import Variable
+
+
+print("ingrese un numero");
+x = int(input());
+if x<100:
+    print("el numero es pequeño");
+else:
+    print("el numero es grande");
---
+++ condicional2.py
@@ -0,0 +1,13 @@
+print("ingrese 2 numeros");
+x = int(input());
+y = int(input());
+if x>y:
+    print("el numero menor es:",y)
+else:
+    if y>x:
+        print("el numero menor es:",x)
+    else:
+        print("los numeros son iguales")
+
+
+
